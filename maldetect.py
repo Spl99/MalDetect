7223fc769fa4afd5d3803270ffdc9a99391cd0ee
Sam <samplong@outlook.com>
2023-05-29 19:59:58 +0100

    Added comments

--- maldetect.py
+++ maldetect.py
@@ -2,6 +2,8 @@ import config
 import requests
 
 search_query = input("Enter search query: ")
+
+# Add your API file below.
 api_key = config.API_KEY
 
 api_url = f"https://api.shodan.io/shodan/host/search?key={api_key}&query={search_query}"
@@ -10,6 +12,7 @@ response = requests.get(api_url)
 
 if response.status_code == 200:
 	data = response.json()
+	# Below checks the search query, returns the IP and Port of the query until nothing else is found
 	if 'matches' in data:
 		for result in data['matches']:
 			ip = result['ip_str']
@@ -18,5 +21,6 @@ if response.status_code == 200:
 		else:
 			print("No search results found.")
 	else:
+		# Below returns the error code if required
 		print(f"Error: {response.status_code} - {response.text}")
 
